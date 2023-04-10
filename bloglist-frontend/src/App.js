26eac6f5bc8ef083f51768ae620985874aaeedf1
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-04-11 00:16:54 +0900

    Complete 5.3/ 5.4

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -8,6 +8,13 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [feedbackMessage, setFeedbackMessage] = useState(null)
+
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -20,7 +27,7 @@ const App = () => {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
-      // blogService.setToken(user.token)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -37,13 +44,17 @@ const App = () => {
         'loggedBlogListUser', JSON.stringify(user)
       )
 
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
-      // setErrorMessage('Wrong credentials')
+      setFeedbackMessage({
+        message: 'Wrong username or password',
+        type: 'error'
+      })
       setTimeout(() => {
-        // setErrorMessage(null)
+        setFeedbackMessage(null)
       }, 5000)
     }
   }
@@ -52,6 +63,45 @@ const App = () => {
     window.localStorage.removeItem('loggedBlogListUser')
     setUser(null)
   }
+
+  const addBlog = async (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: title,
+      author: author,
+      url: url
+    }
+
+    try {
+      const returnedBlog = await blogService.create(blogObject)
+      setBlogs(blogs.concat(returnedBlog))
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+      setFeedbackMessage({
+        message: `${returnedBlog.title} by ${returnedBlog.author} added`,
+        type: 'success'
+      })
+      setTimeout(() => {
+        setFeedbackMessage(null)
+      }, 5000)
+    } catch (error) {
+      setFeedbackMessage({
+        message: error.message,
+        type: 'error'
+      })
+      setTimeout(() => {
+        setFeedbackMessage(null)
+      }, 5000)
+    }
+  }
+
+  const notification = () => (
+    <div className={`feedback ${feedbackMessage.type} `}>
+        {feedbackMessage.message}
+    </div>
+  )
+
 
   const loginForm = () => (
     <>
@@ -80,20 +130,59 @@ const App = () => {
     </>
   )
 
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <div>
+        title:
+          <input
+          type="text"
+          value={title}
+          name="Title"
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        author:
+          <input
+          type="text"
+          value={author}
+          name="Author"
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url:
+          <input
+          type="text"
+          value={url}
+          name="Url"
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+      <button type="submit">Create</button>
+    </form>
+  )
+
   return (
     <div>
+      {feedbackMessage && notification()}
 
-    {user === null ?
-      loginForm() :
-      <>
-        <p>{user.name} logged in</p>
-        <button onClick={handleLogout}>logout</button>
-        <h2>blogs</h2>
-        {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
-        )}
-      </>
-    }
+      {user === null ?
+        loginForm() :
+        <>
+          <p>{user.name} logged in</p>
+          <button onClick={handleLogout}>logout</button>
+
+          <h2>Create new entry</h2>
+
+          {blogForm()}
+
+          <h2>Blogs</h2>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </>
+      }
 
     </div>
   )
